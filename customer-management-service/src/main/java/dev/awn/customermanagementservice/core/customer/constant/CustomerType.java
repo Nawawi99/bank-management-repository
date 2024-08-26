4b08ac12882b946062e5b919cd094a5516f2f8a6
nawawi99 <ahmadwaelnawawi123@gmail.com>
2024-08-27 01:33:42 +0300

    removed unused isValid method

--- customer-management-service/src/main/java/dev/awn/customermanagementservice/core/customer/constant/CustomerType.java
+++ customer-management-service/src/main/java/dev/awn/customermanagementservice/core/customer/constant/CustomerType.java
@@ -3,14 +3,5 @@ package dev.awn.customermanagementservice.core.customer.constant;
 public enum CustomerType {
     RETAIL,
     CORPORATE,
-    INVESTMENT;
-
-    public static boolean isValid(String type) {
-        for (CustomerType t : CustomerType.values()) {
-            if (t.name().equals(type)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    INVESTMENT
 }
